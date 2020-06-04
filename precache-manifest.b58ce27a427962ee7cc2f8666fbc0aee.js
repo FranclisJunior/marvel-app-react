85818b2532ed4252e792d73b589667d31aa56f9b
Franclis Galdino da Silva <franclis.silva@virtus.ufcg.edu.br>
2020-06-03 22:04:32 -0300

    Updates

---
+++ precache-manifest.b58ce27a427962ee7cc2f8666fbc0aee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58f670a563324cfad6582f5eefda9b84",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2082422be44abdb7e332",
+    "url": "/static/css/main.11c9b09c.chunk.css"
+  },
+  {
+    "revision": "c8261a1695502056ec10",
+    "url": "/static/js/2.31fd0395.chunk.js"
+  },
+  {
+    "revision": "4fdb92f5a0a11be5d79ae606cb253c2b",
+    "url": "/static/js/2.31fd0395.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2082422be44abdb7e332",
+    "url": "/static/js/main.de790994.chunk.js"
+  },
+  {
+    "revision": "88869db8947ab536353b",
+    "url": "/static/js/runtime-main.1a8cd47e.js"
+  }
+]);
