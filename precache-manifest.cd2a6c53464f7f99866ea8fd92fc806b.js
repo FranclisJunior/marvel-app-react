b2eac31a8e1890d5d539ff190e5d2f2402778eb5
Franclis Galdino da Silva <franclis.silva@virtus.ufcg.edu.br>
2020-06-03 21:50:18 -0300

    Updates

---
+++ precache-manifest.cd2a6c53464f7f99866ea8fd92fc806b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3922faf47e4ab9c03abc5600ed91a357",
+    "url": "/marvel-app-react/index.html"
+  },
+  {
+    "revision": "aae53acfbbfe22dec3b0",
+    "url": "/marvel-app-react/static/css/main.11c9b09c.chunk.css"
+  },
+  {
+    "revision": "8998225e8618799fcb0c",
+    "url": "/marvel-app-react/static/js/2.3ab2e956.chunk.js"
+  },
+  {
+    "revision": "4fdb92f5a0a11be5d79ae606cb253c2b",
+    "url": "/marvel-app-react/static/js/2.3ab2e956.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aae53acfbbfe22dec3b0",
+    "url": "/marvel-app-react/static/js/main.4fcbce93.chunk.js"
+  },
+  {
+    "revision": "ff9b5c18361651ea4fcf",
+    "url": "/marvel-app-react/static/js/runtime-main.6725ee53.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c3422dfae0d7305625c4d8351703bf70.js"
+  "/marvel-app-react/precache-manifest.cd2a6c53464f7f99866ea8fd92fc806b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/marvel-app-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
